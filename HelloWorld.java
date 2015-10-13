68c0c35ec39455bba5f6a495ba4cbe465890f514
saquiba <saquiba@sbcglobal.net>
2015-10-12 22:40:25 -0700

    Update and rename HelloWorld.java to HelloWorld.cpp

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-	
-	   public static void main(String[] args) {
-	       
-	       //error on purpose: missing semi colon below
-	       System.out.println("Hello World!!!")
-	   }
-
-}
