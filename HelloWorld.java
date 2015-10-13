aa80f4be12a816381ba2a660266fd05a644bd38e
saquiba <saquiba@sbcglobal.net>
2015-10-12 22:16:04 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	
+	   public static void main(String[] args) {
+	       
+	       //error on purpose: missing semi colon below
+	       System.out.println("Hello World!!!")
+	   }
+
+}
